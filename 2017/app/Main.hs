4d4b37f20d5a280a68e0af162ca849ffc1afde5b
Kevin Orr <kevinorr54@gmail.com>
2020-12-01 04:43:48 -0500

    Make 2017 executable

--- 2017/app/Main.hs
+++ 2017/app/Main.hs
@@ -1,8 +1,17 @@
 module Main where
 
 import Data.Char
-
 import qualified Day01
+import System.Environment
+import System.Exit
+  ( ExitCode
+      ( ExitFailure
+      ),
+    exitWith,
+  )
+import Text.Read
+  ( readMaybe,
+  )
 
 newtype Day = Day Int
 
@@ -13,26 +22,27 @@ getRunner (Day 1) One = Just Day01.run1
 getRunner _ _ = Nothing
 
 parseDay :: String -> Either String Day
-parseDay s@(read @Int -> Just n)
+parseDay s@(readMaybe -> Just n)
   | n >= 1 && n <= 25 = Right $ Day n
-  -- | otherwise = Right $ "Not an integer between 1 and 25: " <> s
-parseDay _ = Left $ "Not an integer between 1 and 25: " <> s
+  -- otherwise = Right $ "Not an integer between 1 and 25: " <> s
+parseDay s = Left $ "Not an integer between 1 and 25: " <> s
 
 parsePart :: String -> Either String Part
 parsePart s
-  | toLower s `elem` ["1", "one"] = Right One
-  | toLower s `elem` ["2", "two"] = Right Two
+  | (toLower <$> s) `elem` ["1", "one"] = Right One
+  | (toLower <$> s) `elem` ["2", "two"] = Right Two
   | otherwise = Left $ "Not \"one\" (or \"1\") or \"two\" (or \"2\"): " <> show s
 
 main :: IO ()
 main = do
   executable <- getProgName
   args <- getArgs
-  if length args != 2 then do
-    putStrLn "USAGE: " <> executable <> " DAY PART"
-    exitWith 1
-  else do
-    let Right day = parseDay (args ! 0)
-        Right part = parsePart (args ! 1)
-        Just runner = getRunner day part
-    getContents >>= putStrLn . runner
+  if length args /= 2
+    then do
+      putStrLn $ "USAGE: " <> executable <> " DAY PART"
+      exitWith $ ExitFailure 1
+    else do
+      let Right day = parseDay (args !! 0)
+          Right part = parsePart (args !! 1)
+          Just runner = getRunner day part
+      getContents >>= putStrLn . runner
