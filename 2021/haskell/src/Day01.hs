5c2e0f01b0d4342483856ee08416766bf76fb34c
Kevin Orr <kevinorr54@gmail.com>
2021-12-02 11:16:28 -0500

    2021/01

--- 2021/haskell/src/Day01.hs
+++ 2021/haskell/src/Day01.hs
@@ -1,12 +1,16 @@
 module Day01 where
 
-import qualified Data.Map.Strict as M
-import qualified Data.HashMap.Strict as HM
-import qualified Data.Set as S
-import qualified Data.HashSet as HS
+parse :: String -> [Int]
+parse = fmap read . lines
+
+sliding3sum :: [Int] -> [Int]
+sliding3sum = fmap (\(a, b, c) -> a + b + c) . (\ds -> zip3 ds (tail ds) (tail $ tail ds))
+
+countIncreasing :: [Int] -> Int
+countIncreasing = length . filter (uncurry (<)) . (\ds -> zip ds (tail ds))
 
 part1 :: String -> String
-part1 = show . id . fmap (read @Int) . lines
+part1 = show . countIncreasing . parse
 
 part2 :: String -> String
-part2 = show . id . fmap (read @Int) . lines
+part2 = show . countIncreasing . sliding3sum . parse
